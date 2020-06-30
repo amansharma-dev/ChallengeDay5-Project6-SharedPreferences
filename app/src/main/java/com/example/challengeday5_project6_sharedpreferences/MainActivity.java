0dcb536423ccb8a65932a26449f2f6e42def49a9
amansharma-dev <amansharma.11@hotmail.com>
2020-06-30 10:34:32 +0530

    commit

--- app/src/main/java/com/example/challengeday5_project6_sharedpreferences/MainActivity.java
+++ app/src/main/java/com/example/challengeday5_project6_sharedpreferences/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.challengeday5_project6_sharedpreferences;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -11,8 +12,10 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
 
+    public static final String MESSAGE_ID = "message_sharedPref";
     private EditText editText;
     private Button button;
+    private TextView previousUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,12 +24,27 @@ public class MainActivity extends AppCompatActivity {
 
         editText = findViewById(R.id.editText);
         button = findViewById(R.id.saveButton);
+        previousUser = findViewById(R.id.previousUserName);
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                String userName = editText.getText().toString().trim();
 
+                SharedPreferences sharedPreferences = getSharedPreferences(MESSAGE_ID,MODE_PRIVATE);
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.putString("userName",userName);
+                editor.apply(); // save to disk
+
+                previousUser.setText(userName);
             }
         });
+
+        //get the saved user name
+
+        SharedPreferences getSharedPref = getSharedPreferences(MESSAGE_ID,MODE_PRIVATE);
+        String getPreviousUser = getSharedPref.getString("userName","Nothing Yet...!!");
+        previousUser.setText(getPreviousUser);
+
     }
 }
