13e872006eea65f5c8a016f70fa70f6a90841651
amansharma-dev <amansharma.11@hotmail.com>
2020-06-30 09:45:28 +0530

    commit

--- app/src/main/java/com/example/challengeday5_project6_sharedpreferences/MainActivity.java
+++ app/src/main/java/com/example/challengeday5_project6_sharedpreferences/MainActivity.java
@@ -3,12 +3,30 @@ package com.example.challengeday5_project6_sharedpreferences;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private EditText editText;
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editText = findViewById(R.id.editText);
+        button = findViewById(R.id.saveButton);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
